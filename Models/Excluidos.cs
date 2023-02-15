b38836361ba482d1849b0cbaae8e370d381cb5b9
Yuneno <111202711+Yuneno@users.noreply.github.com>
2023-02-15 12:57:49 -0400

    puesto modelo

---
+++ Models/Excluidos.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace ProyectoADESS.Models
+{
+	public class Excluidos
+	{
+		public int Id { get; set; }
+		public string? Cedula { get; set; }
+		public string? Motiov { get; set; }
+		public string? fecha { get; set; }
+
+
+	}
+}
+
