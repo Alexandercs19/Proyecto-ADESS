5a269d886a78766074c7f5281ca1f4386595bab6
Alexandercs19 <deivialexander6@gmail.com>
2023-02-15 12:12:12 -0400

    Commit

--- Models/add_list.cs
+++ Models/add_list.cs
@@ -2,5 +2,13 @@
 {
     public class add_list
     {
+        public int id_add { get; set; }
+        public string? cedula_add { get; set; }
+        public string? apellido { get; set; }
+        public string? nombre { get; set; }
+        public string? sub { get; set; }
+        public string? monto { get; set; }
+        public string? fecha_add { get; set; }
+            
     }
 }
