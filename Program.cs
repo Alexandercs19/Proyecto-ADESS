c60db0151791531cf6fe020b1d6e992bc580b6b8
Alexandercs19 <deivialexander6@gmail.com>
2023-02-20 22:12:22 -0400

    Commit

--- Program.cs
+++ Program.cs
@@ -24,6 +24,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=MantenedorIncluidos}/{action=Listar}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
