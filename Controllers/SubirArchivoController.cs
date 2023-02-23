ec57943b3127abcaec46474d8f701e7f5624f325
Alexandercs19 <deivialexander6@gmail.com>
2023-02-22 21:59:02 -0400

    Commit

---
+++ Controllers/SubirArchivoController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Web;
+using System.IO;
+using System.Data.SqlClient;
+using System.Data;
+namespace ProyectoADESS.Controllers
+{
+    public class SubirArchivoController : Controller
+    {
+        static string cadena = "Data Source=(Local);Initial Catalog=ADESS;Integrated Security=true";
+
+        public IActionResult Subir()
+        {
+            return View();
+        }
+
+        public ActionResult index() 
+        {
+            return View();
+        }
+        
+        
+    }
+}
